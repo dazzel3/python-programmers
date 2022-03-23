1f618a9eed9a65f4eba16990281c3ffab18d6269
dazzel3 <aakk9350@naver.com>
2022-03-23 18:13:54 +0900

    부족한 금액 계산하기

---
+++ level1/CalShortfall.py
@@ -0,0 +1,8 @@
+def solution(price, money, count):
+    total = 0
+    for i in range(1, count+1):
+        total += price*i
+    if (total - money) > 0:
+        return total - money
+    else:
+        return 0
