c5849d5fcfda5527fe82bb7530a5e5557b0afd49
dazzel3 <aakk9350@naver.com>
2022-03-30 13:31:46 +0900

    두 개 뽑아서 더하기

---
+++ level1/SumTwoNum.py
@@ -0,0 +1,9 @@
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            num = numbers[i] + numbers[j]
+            if num not in answer:
+                answer.append(num)
+    answer.sort()
+    return answer
