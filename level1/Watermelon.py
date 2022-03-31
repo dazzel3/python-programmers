930dd4dd63c2d59c503f5e828d5813614e537e3c
dazzel3 <aakk9350@naver.com>
2022-03-31 22:37:18 +0900

    수박수박수박수박수박수?

---
+++ level1/Watermelon.py
@@ -0,0 +1,8 @@
+def solution(n):
+    answer = ''
+    for i in range(0, n):
+        if i % 2 == 0:
+            answer = answer + '수'
+        else:
+            answer = answer + '박'
+    return answer
